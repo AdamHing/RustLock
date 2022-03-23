6fe8587f74fe48fbf788592cefe1c4d9ef5a3108
Adam Hing <adam.hing@outlook.com>
2022-03-23 15:30:58 -0700

    working version of passworder thing

--- autogui.py
+++
@@ -1,8 +0,0 @@
-import win32com.client as client
-import time
-
-
-wsh = client.Dispatch('WScript.Shell')
-wsh.AppActivate("RustClient.exe")
-time.sleep(4)
-wsh.SendKeys('u are gay')
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from GameInteract import message_sender as ms
 import time 
+import pyautogui as pag
+
+import pandas as pd
+import math
+
+
 kb = ms.keyboard_INPUT()
 
-time.sleep(5)
-kb.KEY_press("w")
-time.sleep(4)
-kb.KEY_release("w")
 
+df = pd.read_csv("codes.csv", dtype = str)
+# first_column = df.iloc[:, 0]
+time.sleep(5)
+
+
+for a in df["code"]:
+    a = (a.zfill(4))
+    # print(a)
+
+# seperation of string into vareiables
+
+# asplit = a.split()
+
+    asplit = list(map(str, a))
+    print(asplit)
+    
+    
+    for i in asplit:
+        
+        
+        # pag.write(i)
+
+        #=-=================================== it do type numbers
+        
+        kb.KEY_press(i)
+        time.sleep(0.5)
+        kb.KEY_release(i)
+        print(i)
+
+    input()
