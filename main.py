20d5e3cbb56ae7472624e119c390962ea9f6d49d
Adam Hing <adam.hing@outlook.com>
2022-03-23 16:19:16 -0700

    cleaned up some stuffs

--- main.py
+++ main.py
@@ -1,45 +1,27 @@
 from GameInteract import message_sender as ms
 import time 
-import pyautogui as pag
-
 import pandas as pd
-import math
 import keyboard
 
-
 kb = ms.keyboard_INPUT()
-
-
 df = pd.read_csv("codes.csv", dtype = str)
-# first_column = df.iloc[:, 0]
 time.sleep(5)
 
 
 for a in df["code"]:
     a = (a.zfill(4))
-    # print(a)
 
-# seperation of string into vareiables
-
-# asplit = a.split()
 
     asplit = list(map(str, a))
     print(asplit)
     
     
     while True:
-
         if keyboard.read_key() == "p":
             print("You pressed p")
             break
-
     for i in asplit:
-        
-        
-        # pag.write(i)
-
-        #=-=================================== it do type numbers
-        
+#=-=================================== it do type numbers  
         kb.KEY_press(i)
         time.sleep(0.5)
         kb.KEY_release(i)
@@ -49,4 +31,3 @@ for a in df["code"]:
 
 
 
-# keyboard.on_presskey("r", lambda :print("You pressed r"))pppp
